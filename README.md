869d748f99f8362165e8eb6a1dd519f457e317e6
zsq <450952271@qq.com>
2019-03-15 15:41:27 +0800

    安装iview插件

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-# hui-iviewhtml
+# zsqtony.github.io
 
-# （vue2.5+） + iview3.0的集合后台管理系统
-## 快速搭建起后台ui，无需再设计
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <router-view/>
-    <BackTop></BackTop>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If iView is successfully added to this project, you'll see an
+        <code v-text="'<Button>'"></code>
+        below
+      </p>
+      <Button type="primary">Button</Button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  color: #2c3e50;
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
-.courselistImg{width:100px;height:80px;line-height:80px;margin-bottom:5px;text-align:center;border:1px solid #f1f1f1;overflow:hidden;img{width:auto;height:80px;}}
-#container{width:100%;height:400px;position:relative;z-index:1;}
-.left_s{float:left;}
-.right_s{float:right;}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
